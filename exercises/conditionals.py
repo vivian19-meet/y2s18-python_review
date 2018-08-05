a14719f2ace5b4e198aeb2665d6805e2618d2989
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 16:32:34 +0300

    done

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,5 @@
 # Write your solution for 1.2 here!
+for i in range(100):
+	if(i%2==0):
+		print(i)
+	
--- exercises/for_loops.py
+++ exercises/for_loops.py
@@ -1,1 +1,7 @@
 # Write your solution for 1.1 here!
+
+y=0
+for i in range(101):
+	y=i+y
+	
+print(y)
